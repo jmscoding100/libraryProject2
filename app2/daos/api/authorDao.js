67840411b36325d565b37f55e01feefa3c0702e2
jmscoding100 <joshua.z.mcgee@gmail.com>
2025-05-20 22:28:16 -0500

    commit-7

---
+++ app2/daos/api/authorDao.js
@@ -0,0 +1,51 @@
+const con = require('../../config/dbconfig')
+
+
+const authorDao ={
+    table: 'author',
+
+    sort:(res, table)=>{
+        con.query(
+            `SELECT * FROM author
+            ORDER BY author_name;`,
+            (error, rows) =>{
+                if(!error){
+                    if(rows.length === 1){
+                        res.json(...rows)
+                    } else {
+                        res.json(rows)
+                    }
+                } else {
+                    console.log(console.log(`DAO ERROR: ${table}`, error))
+                }
+            }
+        )
+    },
+
+    findBookByAuthor:(res, table, author)=>{
+        con.query(
+            `SELECT b.book_id, b.title, a.author_id, p.pub_id, b.copyright_year, b.edition, b.edition_year, f.format_id, b.binding, b.rating, b.language, b.num_pages, b.cover_image, b.qty
+            FROM book b
+            JOIN author a USING (author_id)
+            JOIN publishing_company p USING (pub_id)
+            JOIN format f using (format_id)
+            WHERE a.author_name = '${author}'
+            ORDER BY b.title;
+            `,
+            (error, rows) =>{
+                if(!error) {
+                    res.json(rows)
+                } else {
+                    console.log(`DAO ERROR : ${table}`, error)
+                }
+            }
+        )
+    },
+
+
+    
+
+}
+
+
+module.exports = authorDao
--- app2/daos/api/publishingDao.js
+++ app2/daos/api/publishingDao.js
@@ -1,13 +1,11 @@
 const con = require('../../config/dbconfig')
+const { purge } = require('../../routes/router')
 
-
-const authorDao ={
-    table: 'author',
-
-    sort:(res, table)=>{
+const publishingDao = {
+    table: 'publishing_company',
+    findPublisher:(res, table)=>{
         con.query(
-            `SELECT * FROM author
-            ORDER BY author_name;`,
+            `SELECT * FROM publishing_company;`,
             (error, rows) =>{
                 if(!error){
                     if(rows.length === 1){
@@ -22,30 +20,48 @@ const authorDao ={
         )
     },
 
-    findBookByAuthor:(res, table, author)=>{
+    sort:(res, table)=>{
         con.query(
-            `SELECT b.book_id, b.title, a.author_id, p.pub_id, b.copyright_year, b.edition, b.edition_year, f.format_id, b.binding, b.rating, b.language, b.num_pages, b.cover_image, b.qty
-            FROM book b
-            JOIN author a USING (author_id)
-            JOIN publishing_company p USING (pub_id)
-            JOIN format f using (format_id)
-            WHERE a.author_name = '${author}'
-            ORDER BY b.title;
-            `,
+            `SELECT * FROM publishing_company
+            ORDER BY company_name;`,
             (error, rows) =>{
-                if(!error) {
-                    res.json(rows)
+                if(!error){
+                    if(rows.length === 1){
+                        res.json(...rows)
+                    } else {
+                        res.json(rows)
+                    }
                 } else {
-                    console.log(`DAO ERROR : ${table}`, error)
+                    console.log(console.log(`DAO ERROR: ${table}`, error))
                 }
             }
         )
     },
 
-
-    
+    findBookByPublisher:(res, table, publishing_company)=>{
+        con.query(
+            `SELECT b.book_id, b.title, a.author_id, p.pub_id, b.copyright_year, b.edition, b.edition_year, f.format_id, b.binding, b.rating, b.language, b.num_pages, b.cover_image, b.qty
+            FROM book b
+            JOIN author a USING (author_id)
+            JOIN publishing_company p USING (pub_id)
+            JOIN format f USING (format_id)
+            WHERE p.company_name = '${publishing_company}'
+            ORDER BY b.title;`,
+            (error, rows) =>{
+                if(!error){
+                    if(rows.length === 1){
+                        res.json(...rows)
+                    } else {
+                        res.json(rows)
+                    }
+                } else {
+                    console.log(console.log(`DAO ERROR: ${table}`, error))
+                }
+            }
+        )
+    }
 
 }
 
 
-module.exports = authorDao
+module.exports = publishingDao
